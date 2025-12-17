8c0a79cdad3e6864ec2e084abe4fdbf609ab92b1
Reaven23 <adrien.regis@gmail.com>
2025-12-17 19:31:50 +0100

    alert timeout changed and logo changed

---
+++ app/javascript/controllers/alert_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    delay: { type: Number, default: 4000 }
+  }
+
+  connect() {
+    this.timeout = setTimeout(() => {
+      this.dismiss()
+    }, this.delayValue)
+  }
+
+  disconnect() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  dismiss() {
+    this.element.classList.remove("show")
+    this.element.classList.add("fade-out")
+
+    setTimeout(() => {
+      this.element.remove()
+    }, 300)
+  }
+}
